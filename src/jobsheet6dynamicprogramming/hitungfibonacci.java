f0115d05119de91a795a75c09aa793ec755351ec
ASUS <ASUS@LAPTOP-HV0JURP4>
2018-09-13 19:42:11 +0700

    Initial commit

---
+++ src/jobsheet6dynamicprogramming/hitungfibonacci.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet6dynamicprogramming;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class hitungfibonacci {
+    public static void main(String[] args){
+        
+        String identitas = "Regita Putri Rozi/ X RPL 5/ 35";
+        tampilJudul(identitas);
+        
+    }
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas: " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ..., dst.\n ");
+    }
+}
+    
+        
